fdc0ef95cbfe07c5d3d50f53bb2abde160ff5a1d
Deodatuss <serbgan03@gmail.com>
2023-03-24 20:11:11 +0200

    add Scale method to Vector3D and refactor

--- RayCasting/Structures/Vector3D.cs
+++ RayCasting/Structures/Vector3D.cs
@@ -3,8 +3,8 @@ public class Vector3D:I3DSpaceplacable
     public float X {get; private set;}
     public float Y {get; private set;}
     public float Z {get; private set;}
-
     public float AbsoluteValue {get; private set;}
+
 
     public Vector3D (float X, float Y, float Z)
     {
@@ -14,42 +14,24 @@ public class Vector3D:I3DSpaceplacable
         AbsoluteValue = CalculateAbsoluteValue();
     }
 
-    private float CalculateAbsoluteValue()
-    {
-        return (float)Math.Sqrt(X * X + Y * Y + Z * Z);
-    }
 
-    public Vector3D Normalize()
-    {
-        if (AbsoluteValue != 1)
-        {
-            X /= AbsoluteValue;
-            Y /= AbsoluteValue;
-            Z /= AbsoluteValue;
-            AbsoluteValue = 1;
-        }
-
-        return this;
-    }
-
-    public bool IsNormalized()
-    {
-        return AbsoluteValue == 1 ? true : false;
-    }
+    private float CalculateAbsoluteValue() => 
+    (float)Math.Sqrt(X * X + Y * Y + Z * Z);
 
 
-    public static Vector3D operator +(Vector3D vector1, Vector3D vector2)
-    {
-        return new Vector3D(vector1.X+vector2.X, vector1.Y+vector2.Y, vector1.Z+vector2.Z);
-    }
-        public static Vector3D operator -(Vector3D vector1, Vector3D vector2)
-    {
-        return new Vector3D(vector1.X-vector2.X, vector1.Y-vector2.Y, vector1.Z-vector2.Z);
-    }
-        public static Vector3D operator -(Vector3D vector)
-    {
-        return new Vector3D(-vector.X, -vector.Y, -vector.Z);
-    }
+    public Vector3D Normalize() =>
+    new Vector3D(X/AbsoluteValue, Y/AbsoluteValue, Z/AbsoluteValue);
+    public bool IsNormalized() => 
+    AbsoluteValue == 1 ? true : false;
+
+
+    public static Vector3D operator +(Vector3D vector1, Vector3D vector2) =>
+    new Vector3D(vector1.X + vector2.X, vector1.Y + vector2.Y, vector1.Z + vector2.Z);
+    public static Vector3D operator -(Vector3D vector1, Vector3D vector2) =>
+    new Vector3D(vector1.X - vector2.X, vector1.Y - vector2.Y, vector1.Z - vector2.Z);
+    public static Vector3D operator -(Vector3D vector) => 
+    new Vector3D(-vector.X, -vector.Y, -vector.Z);
+
 
     public Vector3D CrossProductWith(Vector3D other)
     {
@@ -59,11 +41,10 @@ public class Vector3D:I3DSpaceplacable
 
         return new Vector3D(x, y, z);
     }
-
-    public float DotProductWith(Vector3D other)
-    {
-        return this.X * other.X + this.Y * other.Y + this.Z * other.Z;
-    }
+    public float DotProductWith(Vector3D other) => 
+    this.X * other.X + this.Y * other.Y + this.Z * other.Z;
 
 
+    public Vector3D Scale (int scaleBy) => 
+    new Vector3D(this.X*scaleBy, this.Y*scaleBy, this.Z*scaleBy);
 }
