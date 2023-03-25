051e655161704db4013131aabaa0e0f1c890f5aa
Deodatuss <serbgan03@gmail.com>
2023-03-25 14:26:59 +0200

    simplify Camera3D by fixing position and direction

--- RayCasting/Structures/FixedCamera3D.cs
+++ RayCasting/Structures/FixedCamera3D.cs
@@ -1,17 +1,28 @@
-public class Camera3D
+public class FixedCamera3D
 {
     public Point3D Position {get; private set;}
     public Vector3D Direction {get; private set;} //always normalized through constructor
     public int FieldOfView {get; private set;} //from 0 to 180, vFOV=hFOV
     public float DistanceToScreen {get; private set;}
-
-    public Camera3D(Point3D position, Vector3D direction, int fieldOfView, float distanceToScreen) 
+  
+  
+    public FixedCamera3D(int fieldOfView, float distanceToScreen) 
     {
-        this.Position = position;
-        this.Direction = direction.Normalize();
+        this.Position = new Point3D(0, 0, 0);
+        this.Direction = new Vector3D(1, 0, 0);
         this.FieldOfView = fieldOfView;
         this.DistanceToScreen = distanceToScreen;
     }
+    
+    // constructor with unfixed position and direction makes implementation much harder, so will not be used
+    //
+    // public FixedCamera3D(Point3D position, Vector3D direction, int fieldOfView, float distanceToScreen) 
+    // {
+    //     this.Position = position;
+    //     this.Direction = direction.Normalize();
+    //     this.FieldOfView = fieldOfView;
+    //     this.DistanceToScreen = distanceToScreen;
+    // }
 
     public IScreen<Point3D>? Wow()
     {   
@@ -35,12 +46,17 @@ public class Camera3D
     //returns 4 points representing screen corners, starting upper left corner, clockwise
     public (Point3D, Point3D, Point3D, Point3D) GetScreenCorners()
     {
-
-        double width = this.GetScreenWidth();
-        double height = this.GetScreenHeight();
+        float width = (float)this.GetScreenWidth();
+        float height = (float)this.GetScreenHeight();
 
         Point3D upperLeft, upperRight, lowerRight, lowerLeft;
-        upperLeft = upperRight = lowerRight = lowerLeft = null;
+        Vector3D screenCenter = Direction.Scale(DistanceToScreen);
+
+        upperLeft = new Point3D(screenCenter.X, screenCenter.Y-width/2, screenCenter.Z+height/2);
+        upperRight = new Point3D(screenCenter.X, screenCenter.Y+width/2, screenCenter.Z+height/2);
+        lowerRight = new Point3D(screenCenter.X, screenCenter.Y+width/2, screenCenter.Z-height/2);
+        lowerLeft = new Point3D(screenCenter.X, screenCenter.Y-width/2, screenCenter.Z-height/2);
+
 
         return (upperLeft, upperRight, lowerRight, lowerLeft);
     }
