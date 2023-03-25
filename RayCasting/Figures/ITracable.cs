eb23df2eef6bb5e54e11ab58db427f4acd86ad35
Deodatuss <serbgan03@gmail.com>
2023-03-25 22:56:01 +0200

    change method name

--- RayCasting/Figures/ITracable.cs
+++ RayCasting/Figures/ITracable.cs
@@ -1,7 +1,6 @@
 public interface ITracable
 {
     public bool IsIntersectingWith(Ray3D ray);
-    public Point3D? GetIntersectionWith(Ray3D ray);
-
+    public Point3D? GetIntersectionPointWith(Ray3D ray);
     public Ray3D? GetNormalAtPointWith(Ray3D ray);
 }
