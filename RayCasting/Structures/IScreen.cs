e5b46ebc434fe12bc28d958d8f1ff7b92802bec5
Deodatuss <serbgan03@gmail.com>
2023-03-25 15:00:30 +0200

    add pixel and camera fields for IScreen

--- RayCasting/Structures/IScreen.cs
+++ RayCasting/Structures/IScreen.cs
@@ -1,8 +1,9 @@
-using System.Collections.Generic;
-
 public interface IScreen<T>
 {
-    public int Width {get;}
-    public int Height {get;}
+    public float Width {get;}
+    public float Height {get;}
+    public int NumberOfPixelsByWidth {get;}
+    public int NumberOfPixelsByHeight {get;}
     public T[,] InformationPixels {get;}
+    public ICamera AttachedCamera {get;}
 }
