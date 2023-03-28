86bad3deceedf0e13d989a71f4e0ed25fe5a509c
Deodatuss <serbgan03@gmail.com>
2023-03-28 23:42:41 +0300

    add non-realized Disk figure

---
+++ RayCasting/Figures/Disk.cs
@@ -0,0 +1,17 @@
+public class Disk : ITracable
+{
+    public Point3D? GetIntersectionPointWith(Ray3D ray)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Ray3D? GetNormalAtPointWith(Ray3D ray)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsIntersectingWith(Ray3D ray)
+    {
+        throw new NotImplementedException();
+    }
+}
